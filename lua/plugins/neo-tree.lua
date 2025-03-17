994d7a16ed9f93c05f1ce349ab27cee8eee0344e
Rur1k <moalmatrafi90@gmail.com>
2025-03-17 23:04:58 +0300

    changed some NeoTree settings to make all hidden files visible on startup

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,13 @@ return {
         -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
+        require('neo-tree').setup {
+            filesystem = {
+                filtered_items = {
+                    visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+                }
+            }
+        }
         vim.cmd("Neotree filesystem right reveal ")
         vim.cmd("Neotree focus")
     end,
