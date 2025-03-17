e06ad02ee0a4b2baca7000a41702445f935ef219
Rur1k <moalmatrafi90@gmail.com>
2025-03-17 20:31:47 +0300

    formatted and edited some keybinds

--- lua/config/plugins-keymaps.lua
+++ lua/config/plugins-keymaps.lua
@@ -11,16 +11,16 @@ vim.keymap.set("n", "<leader>p", vim.lsp.buf.format, {})
 
 local cmp = require("cmp")
 cmp.setup({
-                mapping = {
-                    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-                    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }, ["<C-y>"] = cmp.mapping(
-                        cmp.mapping.confirm {
-                            behavior = cmp.ConfirmBehavior.Insert,
-                            select = true,
-                        },
-                        { "i", "c" }
-                    ),
-                },
+    mapping = {
+        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        ["<CR>"] = cmp.mapping(
+            cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Insert, select = true,
+            },
+            { "i", "c" }
+        ),
+    },
 })
 
 local ls = require("luasnip")
