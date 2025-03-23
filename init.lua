891909274e9bc8cc30d3155823ff2d6883f0d55e
Rur1k <moalmatrafi90@gmail.com>
2025-03-23 22:48:15 +0300

    added a nice cursor to insert mode

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@
 this is the file that will execute in the begining of launching vim
 it import every plugin and keymaps
 ]]
-
 require("config.vim-options")
 require("config.keymaps")
 require("config.lazy")
--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -1,3 +1,4 @@
+vim.opt.guicursor = ""
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
